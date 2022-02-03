e7afaa30ec7c9aa26f47b8abbb280f2ae778d4a9
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2022-02-03 08:39:34 -0300

    setup initial project settings

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if (!fontsLoaded) return <AppLoading />
+
+  return (
+    <View>
+
+    </View>
+  );
+}
