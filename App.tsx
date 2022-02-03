d0561d52e841ea9946eb83979f492ec93e2099ee
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2022-02-03 08:50:24 -0300

    setup theme provider from styled components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,8 +15,8 @@ export default function App() {
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <View>
+    <ThemeProvider theme={theme}>
 
-    </View>
+    </ThemeProvider>
   );
 }
