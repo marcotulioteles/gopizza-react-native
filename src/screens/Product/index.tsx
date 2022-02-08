1e676bcbeaca353d290af2c55e0da7ccbd72a224
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2022-02-08 18:43:28 -0300

    created feature to load images from device

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { Photo } from '@components/Photo';
 
 import {
   Container,
   Header,
   Title,
-  DeleteLabel
+  DeleteLabel,
+  PickImageButton,
+  Upload
 } from './styles';
 
 export function Product() {
+  const [image, setImage] = useState('');
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -20,6 +41,15 @@ export function Product() {
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
+
+      <Upload>
+        <Photo uri={image} />
+        <PickImageButton
+          title='Carregar'
+          type='primary'
+          onPress={handlePickerImage}
+        />
+      </Upload>
     </Container>
   )
 }
